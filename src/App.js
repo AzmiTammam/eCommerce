c62188ffc84056db2330fa3c1e36b3492a18ba08
“Azmi <Azmitammam@gmail.com>
2021-12-03 15:48:17 +0200

    added project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { Component } from 'react';
 import './App.css'
 import SHOP_DATA from './shop.data';
 import {Route, Switch,Redirect} from "react-router-dom"
+import NavBar from './components/navbar/NavBar';
+import Footer from './components/footer/footer';
+import HomePage from './pages/home/homepage.component';
+import Register from './components/Sign/Register';
+import Login from './components/Sign/Login';
 
 
 
@@ -11,7 +16,9 @@ class App extends Component {
 
     this.state = {
       items: SHOP_DATA,
-      cartItems: []
+      cartItems: [],
+      currentUser: null,
+
     }
     
   }
@@ -19,7 +26,17 @@ class App extends Component {
     
     return (
       <div className="App">
-
+        <NavBar  />
+        <Switch>
+          <Route exact path="/" >
+            <HomePage items={this.state.items}  />
+          </Route>
+          <Route exact path="/register" >
+            <Register  />
+          </Route>
+          <Route exact path="/login" render={() => this.state.currentUser ?(<Redirect to="/"  />) : (<Login  />) }  />
+        </Switch>
+        <Footer  />
       </div>
     );
   }
