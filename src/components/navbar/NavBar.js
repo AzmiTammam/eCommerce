3af76b4d9985fe314ad01e0b05d01fa3f2c73eab
Msallam-daas <msallam.daas@gmail.com>
2021-12-03 14:26:09 +0200

    navbar&footer

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import "./NavBar.css";
+
+export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  showMenu = () => {
+    let burgerMenu = document.getElementById("navShow");
+
+    if (burgerMenu.style.display === "none") {
+      burgerMenu.style.display = "flex";
+    } else {
+      burgerMenu.style.display = "none";
+    }
+
+    console.log(this);
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <a onClick={this.showMenu}>
+          <i className="fas fa-bars burgerMenu"></i>
+        </a>
+
+        <div className="nav">
+          <div className="navCenter">
+            <li className="navLogo">i C A P</li>
+          </div>
+          <ul className="topList" id="navShow">
+            <li>
+              <a className="active" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#">Shop</a>
+            </li>
+            <li>
+              <a href="#">Cups&Hats</a>
+            </li>
+            <li>
+              <a href="#">Pages</a>
+            </li>
+            <li>
+              <a href="#">REGISTER</a>
+            </li>
+            <li>
+              <a href="#">SIGN IN</a>
+            </li>
+          </ul>
+          <div className="shoppingCart">
+            <ul>
+              <li>
+                <i className="topIcon fas fa-shopping-cart"></i>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
