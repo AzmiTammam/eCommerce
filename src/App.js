f530cbae615d589c54fb7de97807c3c3a48e5f58
Msallam-daas <msallam.daas@gmail.com>
2021-12-04 14:39:51 +0200

    shop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { Component } from 'react';
-import './App.css'
-import SHOP_DATA from './shop.data';
-import {Route, Switch,Redirect} from "react-router-dom"
-import NavBar from './components/navbar/NavBar';
-import Footer from './components/footer/footer';
-import HomePage from './pages/home/homepage.component';
-import SignInPage from './pages/sign-in/sign-in.component';
-
-
+import { Component } from "react";
+import "./App.css";
+import SHOP_DATA from "./shop.data";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
+import Footer from "./components/footer/footer";
+import HomePage from "./pages/home/homepage.component";
+import SignInPage from "./pages/sign-in/sign-in.component";
+import ShopPage from "./pages/shop/shop.components";
 
 class App extends Component {
   constructor(props) {
@@ -17,32 +16,46 @@ class App extends Component {
       items: SHOP_DATA,
       cartItems: [],
       currentUser: null,
-    }
-
-   
+    };
   }
 
-  handleCurrentUser = (userName,password) => {
-    this.setState({currentUser: {username: userName, pass: password}})
-  }
-
+  handleCurrentUser = (userName, password) => {
+    this.setState({ currentUser: { username: userName, pass: password } });
+  };
 
   render() {
-    
     return (
       <div className="App">
         <NavBar currentUser={this.state.currentUser} />
         <Switch>
-          <Route exact path="/" >
-            <HomePage items={this.state.items} currentUser={this.state.currentUser} />
+          <Route exact path="/">
+            <HomePage
+              items={this.state.items}
+              currentUser={this.state.currentUser}
+            />
           </Route>
-          <Route exact path="/login" render={() => this.state.currentUser ? (<Redirect to="/" />) : (<SignInPage  handleCurrentUser={this.handleCurrentUser} />) }  />
+          <Route exact path="/Shop">
+            <ShopPage
+              items={this.state.items}
+              currentUser={this.state.currentUser}
+            />
+          </Route>
+          <Route
+            exact
+            path="/login"
+            render={() =>
+              this.state.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInPage handleCurrentUser={this.handleCurrentUser} />
+              )
+            }
+          />
         </Switch>
-        <Footer  />
+        <Footer />
       </div>
     );
   }
 }
 
 export default App;
-
--- src/components/featured-products/featured-products.component.jsx
+++ src/components/featured-products/featured-products.component.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import "./featured-products.styles.css"
+import React from "react";
+import "./featured-products.styles.css";
 
-const FeaturedProducts = ({item,currentUser}) => {
-    return (
-        <div className="card-container" id="featured-products-section">
-             <div className="featured-product-image" style={{
-               backgroundImage: `url(${item.imageUrl})`
-           }} />
-           <div className="collection-footer">
-               <span className="name">{item.name}</span>
-               <span className="price">${item.price}.00</span>
-           </div>
-           <div className="desc-button">
-           <span className="desc">{item.desc}</span>
-           {currentUser ? <button type="button" className="add-to-cart-button">ADD TO CART</button> : null}
-           </div>
-        </div>
-    )
-}
+const FeaturedProducts = ({ item, currentUser }) => {
+  return (
+    <div className="card-container" id="featured-products-section">
+      <div
+        className="featured-product-image"
+        style={{
+          backgroundImage: `url(${item.imageUrl})`,
+        }}
+      />
 
-export default FeaturedProducts
+      <div className="collection-footer">
+        <span className="name">{item.name}</span>
+        <span className="price">${item.price}.00</span>
+      </div>
+      <div className="desc-button">
+        <span className="desc">{item.desc}</span>
+        {currentUser ? (
+          <button type="button" className="add-to-cart-button">
+            ADD TO CART
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
