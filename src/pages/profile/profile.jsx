54caf18884d5a27ebaefe12236de8062e03008ef
AbdallahMarei <abdullahmari@gmail.com>
2021-12-04 15:48:13 +0200

    edit

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,20 +1,52 @@
 import React, { Component } from "react";
+import Weather from "../../components/Weather/Weather";
 import "./Profile.styles.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export class Profile extends Component {
+  constructor(props){
+    super(props);
+    this.state=""
+  }
   render() {
-    return (
-      <div className="profile">
-        <div className="text">
-          <h1>My Account</h1>
-          <p>
-            Hello, userName Here you can view your personal information and
-            purchases
-          </p>
-        </div>
-        <div className="weather"></div>
-      </div>
-    );
+    const localData = JSON.parse(localStorage.getItem("users"));
+  
+    
+    for (const i in localData) {
+      if (
+        localData[i].username === this.props.currentUser.username &&
+        localData[i].password === this.props.currentUser.pass
+      ) {
+        return (
+          <div className="profile">
+            <div className="text">
+              <h1>My Account</h1>
+              <p>
+                Hello <span style={{fontWeight:"bolder",color:"brown"}}>{localData[i].username}</span> , here you can view your personal information and
+                purchases
+              </p>
+              <div className="personal-info">
+                <h2>Personal Information</h2>
+                <label>Username:</label>
+                <input type="text" value={localData[i].username} disabled />
+                <label>Email:</label>
+                <input type="text" value={localData[i].email} disabled />
+                <label>Phone:</label>
+                <input type="text" value={localData[i].phone} disabled />
+                <label>Country:</label>
+                <input type="text" value={localData[i].country} disabled />
+              </div>
+            </div>
+            <div className="weather"><Weather country={localData[i].country}/> </div>
+          
+          </div>
+        );
+      }
+     
+    }
+   
+
+  
   }
 }
 
