9f2127c54d0d4be9fc316b1dd3366a8daf7ddaf5
AbdallahMarei <abdullahmari@gmail.com>
2021-12-04 15:50:09 +0200

    edit

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -2,50 +2,45 @@ import React, { Component } from "react";
 import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-class Weather extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    async function displayWeather() {
-      let city = document.getElementById("cityName").value;
-      let apiKey = "3025d0777154910572eb91d5d128c969";
+function Weather(props) {
+  async function displayWeather() {
+    // let city = this.props.country;
+    let apiKey = "3025d0777154910572eb91d5d128c969";
 
-      //make request to url
-      const response = await fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-          city +
-          "&appid=" +
-          apiKey
-      );
-      const data = await response.json();
-      console.log(data);
-      document.getElementById("test").innerHTML = 
-        
-      <div class="card mx-auto mt-5" style="width: 18rem;">
-          <div class="card-body justify-content-center">
-              <h5 class="card-title">${data.name}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Highs of ${data.main.temp_max}. Lows of ${data.main.temp_min}</h6>
-              <p class="card-text ">Weather conditions are described as: ${data.weather[0].description}</p>
-              <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
-
-          </div>
-      </div>
-      ;
-    }
-    return (
-      <div>
-        <input type="text" id="cityName" placeholder="Enter a city name" />
-        <br />
-        <input
-          type="submit"
-          value="Get Weather Information"
-          id="weather_data"
-          onClick={displayWeather}
-        />
-        <div id="test"></div>
-      </div>
+    //make request to url
+    const response = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        props.country +
+        "&appid=" +
+        apiKey +
+        "&units=metric"
     );
+    const data = await response.json();
+    console.log(data);
+    // console.log(props.country);
+    document.getElementById(
+      "test"
+    ).innerHTML = ` <div class="card mx-auto mt-5" >
+          <div class="card-body justify-content-center">
+          <div> <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/> </div>
+              <h5 class="card-title">${data.name}</h5>   
+              <span className='temp' >${data.main.temp}</span>     
+              <p class="card-text ">Weather conditions are described as: ${data.weather[0].description}</p>
+              <p className='temp' >humidity: ${data.main.humidity} |  wind: ${data.wind.speed}</p>  
+            
+          </div>
+      </div>`;
+    document.getElementById("btn").style.display = "none";
   }
+  return (
+    <div className="weather-section">
+      <button onClick={displayWeather} id="btn">
+        show weather
+      </button>
+
+      <div id="test"></div>
+    </div>
+  );
 }
+
 export default Weather;
